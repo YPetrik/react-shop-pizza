9d1adb7307064bfddd49181bd74f56950ba53731
Yuriy <yrii.petrik@gmail.com>
2023-04-28 11:53:59 +0700

    14.1_ add focus on input after of remove informaceon

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import { FiSearch, FiX } from 'react-icons/fi';
 
@@ -9,17 +9,25 @@ import style from './Search.module.scss';
 const Search = () => {
   const { searchValue, setSearchValue } = React.useContext(SearchContext);
 
+  const inputRef = useRef();
+
+  const onClickClear = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+  };
+
   return (
     <div className={style.wrapper}>
       <FiSearch className={style.wrapper__icon} />
       <input
+        ref={inputRef}
         className={style.wrapper__input}
         type="text"
         placeholder="Поиск пиццы ..."
         value={searchValue}
         onChange={(e) => setSearchValue(e.target.value)}
       />
-      {searchValue && <FiX className={style.wrapper__delete} onClick={() => setSearchValue('')} />}
+      {searchValue && <FiX className={style.wrapper__delete} onClick={onClickClear} />}
     </div>
   );
 };
