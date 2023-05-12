f91b90bea2a6cc27058bd68c53d3e1a1ba79ba72
Yuriy <yrii.petrik@gmail.com>
2023-05-12 11:58:24 +0700

    19.2_ added in component Outlet + uaeNavigate  react-router-dom

--- src/App.js
+++ src/App.js
@@ -5,24 +5,21 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import NotFound from './pages/NotFound';
-import Header from './components/Header';
 
 import './scss/app.scss';
 import FullPizza from './pages/FullPizza';
+import MainLayout from './layout/MainLayout';
 
 function App() {
   return (
-    <div className="wrapper">
-      <Header />
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/pizza/:id" element={<FullPizza />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
--- src/pages/FullPizza/index.jsx
+++ src/pages/FullPizza/index.jsx
@@ -1,9 +1,10 @@
 import axios from 'axios';
 import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 const FullPizza = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [pizza, setPizza] = React.useState();
 
   React.useEffect(() => {
@@ -12,6 +13,7 @@ const FullPizza = () => {
         const { data } = await axios(`https://641fd25482bea25f6df5717d.mockapi.io/items/${id}`);
         setPizza(data);
       } catch (error) {
+        navigate('/');
         console.log(error);
       }
     };
